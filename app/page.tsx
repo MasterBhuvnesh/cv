9f5b75cde11a7fe19fd82c1fc32d4419c25553f4
Bhuvnesh Verma <bhuvneshverma2904@gmail.com>
2025-08-16 23:48:54 +0530

    chore: add comment for main component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { PortfolioCV } from "@/components/portfolio";
 export default function Page() {
   return (
     <div className="min-h-screen">
+      {/* MAIN COMPONENT */}
       <PortfolioCV />
     </div>
   );
